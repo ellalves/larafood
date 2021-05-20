d6e2fed0d8f7c839be539a6dde4a655cb7268065
ellalves <ellalves@hotmail.com.br>
2021-05-20 11:36:53 -0300

    Módulo de Pedidos em Real-time consertado

--- resources/js/Echo.js
+++ resources/js/Echo.js
@@ -4,9 +4,8 @@ import Bus from './bus'
 // get id tenant
 const tenantId = window.Laravel.tenantId;
 
-window.Echo.private(`larafood_database_private-order-created.${tenantId}`)
+window.Echo.channel(`larafood_database_private-order-created.${tenantId}`)
 .listen('OrderCreated', (e) => {
-    //Bus.$emit('order.created', e.order)
-    alert('oi')
-    // Vue.$vToastify.success(`Novo pedido ${e.order.identify}`, 'Novo Pedido')
+    Bus.$emit('order.created', e.order)
+    Vue.$vToastify.success(`Novo pedido ${e.order.identify}`, 'Novo Pedido')
 });
