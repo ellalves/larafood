8f4d73a4b8b3a3beab3fbed9c253f6d9b2182b72
ellalves <ellalves@hotmail.com.br>
2021-07-16 19:58:25 -0300

    teste

---
+++ resources/lang/ptbr/addresses.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+
+    'title' => 'Endereço|Endereços',
+
+    'all' => 'Todos os endereços',
+    'browse' => 'Procurar endereços',
+    'latest' => 'Últimos endereços',
+    'view' => 'Exibir endereço|Exibir endereços',
+    'new' => 'Novo endereço|Novos endereços',
+    'none' => 'Nenhum endereço encontrado',
+    'search' => 'Pesquisar endereço|Pesquisar endereços',
+
+    'ajax-search' => 'Comece a digitar um endereço ...',
+
+    'autocomplete' => [
+        'placeholder' => 'Comece a digitar um endereço para carregar sugestões ...',
+        'reset' => 'Reset',
+    ],
+
+    'primary' => [
+        'title' => 'Endereço principal|Endereços principais',
+    ],
+
+    'billing' => [
+        'title' => 'Endereço de cobrança|Endereços de cobrança',
+        'new' => 'Novo endereço de cobrança|Novos endereços de cobrança',
+    ],
+
+    'shipping' => [
+        'title' => 'Endereço de entrega|Endereços de entrega',
+        'new' => 'Novo endereço de entrega|Novos endereços de entrega',
+
+        'same-as-billing' => 'O endereço de entrega é o mesmo que o endereço de cobrança',
+    ],
+
+    'delivery' => [
+        'title' => 'Endereço de entrega|Endereços de entrega',
+        'new' => 'Novo endereço de entrega|Novos endereços de entrega',
+
+        'same-as-billing' => 'Endereço de entrega é o mesmo que endereço de cobrança',
+    ],
+
+    'fields' => [
+        'street' => 'Rua e número',
+        'post_code' => 'Código Postal',
+        'city' => 'Cidade',
+        'state' => 'Estado',
+        'country' => 'País',
+
+        'notas' => 'Nota|Notas',
+
+        'route' => 'Rota',
+        'street_number' => 'Número da rua',
+        'street_extra' => 'Informações adicionais de endereço',
+    ],
+
+];
